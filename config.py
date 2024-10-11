81a9849af8549f4c28251c22f393579edc940ebb
yewengood <152235769+yewengood@users.noreply.github.com>
2024-10-11 10:22:21 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,58 @@
+ip_version_priority = "ipv6"
+
+source_urls = [
+    "https://raw.githubusercontent.com/yewengood/gdtv/refs/heads/main/speedtest/gdtv_fofa.txt",
+    "https://raw.githubusercontent.com/yewengood/gdzb/refs/heads/main/speedtest/gddx_fofa.txt",
+    "https://raw.githubusercontent.com/yewengood/gdzb/main/speedtest/zubo_fofa.txt",
+    "https://raw.githubusercontent.com/yewengood/-/main/888.txt",
+    "http://我不是.摸鱼儿.top/live.php",
+    "https://raw.githubusercontent.com/yewengood/ktv/refs/heads/main/speedtest/gdtv_fofa.txt",
+    "http://free.xiptv.x10.bz/p3p.php",
+    "https://raw.githubusercontent.com/yewengood/kzb/main/zhtv.txt"
+    #"https://gitcode.net/ygbh66/666/raw/master/x.txt",
+    #"https://raw.githubusercontent.com/fanmingming/live/main/tv/m3u/ipv6.m3u",
+   # "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
+   # "http://120.79.4.185/new/mdlive.txt",
+   # "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V4.txt",
+   # "https://live.zhoujie218.top/tv/iptv6.txt",
+   # "https://raw.githubusercontent.com/Guovin/TV/gd/result.txt",
+   # "https://raw.githubusercontent.com/vbskycn/iptv/master/tv/hd.txt",
+   # "https://cdn.jsdelivr.net/gh/YueChan/live@main/IPTV.m3u",
+   # "https://raw.githubusercontent.com/cymz6/AutoIPTV-Hotel/main/lives.txt",
+   # "https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/live.txt",
+   # "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt",
+   # "https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt"
+]
+
+url_blacklist = [
+    "epg.pw/stream/",
+    "103.40.13.71:12390",
+    "[2409:8087:1a01:df::4077]/PLTV/",
+    "8.210.140.75:68",
+    "154.12.50.54",
+    "yinhe.live_hls.zte.com",
+    "8.137.59.151",
+    "[2409:8087:7000:20:1000::22]:6060",
+    "histar.zapi.us.kg",
+    "www.tfiplaytv.vip",
+    "dp.sxtv.top",
+    "111.230.30.193",
+    "148.135.93.213:81",
+    "live.goodiptv.club",
+    "iptv.luas.edu.cn"
+]
+announcements = [
+    {
+        "channel": "公告",
+        "entries": [
+            {"name": "更新日期", "url": "https://liuliuliu.tv/api/channels/1997/stream", "logo": "http://175.178.251.183:6689/LR.jpg"},
+            {"name": None, "url": "https://liuliuliu.tv/api/channels/233/stream", "logo": "http://175.178.251.183:6689/LR.jpg"}
+  ]
+    }
+]
+
+epg_urls = [
+    "https://live.fanmingming.com/e.xml",
+    "http://43.136.119.99:4567/epg",
+   
+]
