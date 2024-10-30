07a38083246fdcb912622747d652f9bee6ef347f
yewengood <152235769+yewengood@users.noreply.github.com>
2024-10-30 17:48:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ source_urls = [
     "https://raw.githubusercontent.com/yewengood/kzb/main/zhtv.txt",
     "https://www.huanghuadi.com.cn/live.txt",
     "http://aktv.top/live.m3u",
-    "http://kxrj.site:567/gggg.nzk"
-   # "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
+    "http://kxrj.site:567/gggg.nzk",
+   "http://rihou.cc:567/gggg.nzk"
    # "http://120.79.4.185/new/mdlive.txt",
    # "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V4.txt",
    # "https://live.zhoujie218.top/tv/iptv6.txt",
