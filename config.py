22815804aa68c82e9e766747f491d2b92028780e
yewengood <152235769+yewengood@users.noreply.github.com>
2024-11-15 16:35:55 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ source_urls = [
     "https://raw.githubusercontent.com/yewengood/-/main/888.txt",
     "http://我不是.摸鱼儿.com/live.php",
     "https://raw.githubusercontent.com/yewengood/ktv/refs/heads/main/speedtest/gdtv_fofa.txt",
-    "https://raw.githubusercontent.com/yewengood/kzb/main/zhtv.txt",
     "https://www.huanghuadi.com.cn/live.txt",
     "http://aktv.top/live.m3u",
     "http://kxrj.site:567/gggg.nzk",
